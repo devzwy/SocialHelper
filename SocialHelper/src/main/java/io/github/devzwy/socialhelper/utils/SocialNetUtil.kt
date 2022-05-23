307737739aa03ff8db04c6994f168f1ff282b820
Yumi <dev_zwy@aliyun.com>
2022-05-24 05:56:19 +0800

    10

--- SocialHelper/src/main/java/io/github/devzwy/socialhelper/utils/SocialNetUtil.kt
+++ SocialHelper/src/main/java/io/github/devzwy/socialhelper/utils/SocialNetUtil.kt
@@ -14,7 +14,6 @@ object SocialNetUtil {
 
     /**
      * 发送get请求
-     *
      */
     fun sendGet(url: String, onSucc: (String) -> Unit, onErr: (String) -> Unit) {
         SocialHelper.socialConfig.okHttpClient.newCall(
